997a97ec84cbcdb1a15f17c14532e61550029000
devbox <devbox@devbox-VirtualBox.(none)>
2014-08-06 02:15:18 +0800

    completed forms 7.3.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-
+  resources :users
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
