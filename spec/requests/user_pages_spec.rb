8d69e43b917d92a733b40f515c104a7d5de55b40
Scott Yu <scott.tj.yu@gmail.com>
2014-08-06 15:20:20 +0800

    chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,12 +31,27 @@ describe "User pages" do
       end
     end
 
+    describe "after submission" do
+      before { click_button submit }
+
+      it { should have_title('Sign up') }
+      it { should have_content('error') }
+    end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
       it "should create a user" do
