20a66db8ac4b1fa889b7cc277516687c7fb2fab5
Scott Yu <scott.tj.yu@gmail.com>
2014-08-12 15:23:57 +0800

    11.3.1 the status feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,7 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)    #other_user is just a place holder for the parameter, it can be anything.
