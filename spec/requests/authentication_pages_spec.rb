cb11d72fa1b37aa06e7f1aecc984aec85269f2e8
Scott Yu <scott.tj.yu@gmail.com>
2014-08-13 17:59:35 +0800

    updated rspec test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,17 +7,24 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+
+  describe "signin" do
+    before { visit signin_path }
+
     describe "with invalid information" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
-    end
+      it { should have_error_message('Invalid') }
 
-    describe "after visiting another page" do
-		  before { click_link "Home" }
-		  it { should_not have_selector('div.alert.alert-error') }
-		end
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
@@ -33,6 +40,11 @@ describe "Authentication" do
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 
@@ -40,46 +52,6 @@ describe "Authentication" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
-
-      describe "in the Users controller" do
-
-        describe "visiting the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_title('Sign in') }
-        end
-
-        describe "submitting to the update action" do
-          before { patch user_path(user) }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
-
-        describe "visiting the user index" do
-          before { visit users_path }
-          it { should have_title('Sign in') }
-        end
-
-        describe "submitting to the create action" do
-          before { post microposts_path }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
-
-        describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
-      end
-
-      describe "in the Relationships controller" do
-        describe "submitting to the create action" do
-          before { post relationships_path }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
-
-        describe "submitting to the destroy action" do
-          before { delete relationship_path(1) }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
-      end
 
       describe "when attempting to visit a protected page" do
         before do
@@ -109,6 +81,11 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
 
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
         describe "visiting the following page" do
           before { visit following_user_path(user) }
           it { should have_title('Sign in') }
@@ -117,6 +94,31 @@ describe "Authentication" do
         describe "visiting the followers page" do
           before { visit followers_user_path(user) }
           it { should have_title('Sign in') }
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
     end
@@ -137,6 +139,7 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }  #should be redirected to the root path
       end
     end
+
 
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
