53146e0f5ff694dacfc0ab2eefee0b3c75c77047
Scott Yu <scott.tj.yu@gmail.com>
2014-10-10 18:58:44 +0800

    update tracked files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in @user
+      sign_in @user                 
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to @user                 #@user this case is the user_params, which is the user URL.
     else
       render 'new'
     end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -55,7 +55,7 @@ describe "Authentication" do
 
       describe "when attempting to visit a protected page" do
         before do
-          visit edit_user_path(user)
+          visit edit_user_path(user)                       #because of the signed_in_user? before_action. Visiting the edit papge will be directed to the signin page.
           fill_in "Email",    with: user.email
           fill_in "Password", with: user.password
           click_button "Sign in"
